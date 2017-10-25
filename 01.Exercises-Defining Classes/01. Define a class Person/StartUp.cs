e45304db9c83e762f99e9df266eb7fd99ad489ff
tkasakov <tihomirkasakov@gmail.com>
2017-10-25 15:41:51 +0300

    Added 1,2,3 Task

---
+++ 01.Exercises-Defining Classes/01. Define a class Person/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+public class StartUp
+{
+    static void Main()
+    {
+        Type personType = typeof(Person);
+        PropertyInfo[] properties = personType.GetProperties
+            (BindingFlags.Public | BindingFlags.Instance);
+        Console.WriteLine(properties.Length);
+    }
+}
---
+++ 01.Exercises-Defining Classes/03. Opinion Poll/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class StartUp
+{
+    static void Main()
+    {
+        var personCount = int.Parse(Console.ReadLine());
+        List<Person> persons = new List<Person>();
+
+        for (int i = 0; i < personCount; i++)
+        {
+            string [] tokens = Console.ReadLine().Split();
+            string name = tokens[0];
+            int age = int.Parse(tokens[1]);
+            Person nameAge = new Person(name,age);
+            persons.Add(nameAge);
+        }
+
+        List<Person> filter = persons.Where(p => p.Age > 30).OrderBy(p => p.Name).ToList();
+
+        foreach (Person person in filter)
+        {
+            Console.WriteLine($"{person.Name} - {person.Age}");
+        }
+    }
+}
